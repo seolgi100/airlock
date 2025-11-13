40b20950802cdf34a9f98c73ec600bb7639613bc
seolgi <seolgi100k@gmail.com>
2025-11-13 23:12:06 +0900

    [backend] refactor: remove unused Room and RoomMember entities

--- backend/src/main/java/com/airlock/backend/domain/repository/RoomRepository.java
+++
@@ -1,8 +0,0 @@
-package com.airlock.backend.domain.repository;
-
-import com.airlock.backend.domain.entity.Room;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoomRepository extends JpaRepository<Room, Long> {
-
-}
