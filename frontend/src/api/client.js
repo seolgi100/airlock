6e8449231b43b7bdb0a7e39aecbccfc9173d6307
Jihyeon Song <songsong7878@naver.com>
2025-11-19 19:28:42 +0900

    [front] edit: client.js 수정

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,10 +1,12 @@
 // src/api/client.js
 import axios from "axios";
 
-const API_BASE_URL =
-  window.location.hostname === "localhost"
-    ? ""                           
-    : `https://${window.location.hostname}`;
+const isLocal = window.location.hostname === "localhost";
+
+// 로컬에서도, 서버에서도 동일하게 15.165.2.31:8080 API 서버
+const API_BASE_URL = isLocal
+  ? "http://15.165.2.31:8080"
+  : "http://15.165.2.31:8080";
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
