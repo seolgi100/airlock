085242803a602f75b6d8703e4b5ee9cdb663365e
Jihyeon Song <songsong7878@naver.com>
2025-11-23 01:42:13 +0900

    [front] edit: 서버 IP 변경 반영

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,15 +1,13 @@
 // src/api/client.js
 import axios from "axios";
 
-const isLocal = window.location.hostname === "localhost";
-
-// 로컬에서도, 서버에서도 동일하게 43.202.212.164:8080 API 서버
-const API_BASE_URL = isLocal
-  ? "http://43.202.212.164:8080"
-  : "http://43.202.212.164:8080";
+const API_BASE_URL =
+  window.location.hostname === "localhost"
+    ? "http://localhost:8080"
+    : "http://43.202.212.164:8080";
 
 const apiClient = axios.create({
-  baseURL: "/api",
+  baseURL: API_BASE_URL,
   withCredentials: true,
 });
 
