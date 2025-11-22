381adf1e655df3512a023807aaa7dcde48d6eff1
HyeJinYoon1226 <younzzippong@gmail.com>
2025-11-22 11:13:17 +0900

    import HttpServletRequest 추가

--- backend/src/main/java/com/airlock/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/airlock/backend/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.airlock.backend.config;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
