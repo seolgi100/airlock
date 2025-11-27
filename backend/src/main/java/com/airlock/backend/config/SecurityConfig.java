ca6dd7e6b9860def689bbe7b965ff7599509e032
seolgi <seolgi100k@gmail.com>
2025-11-27 20:22:08 +0900

    [back] fix: WebSecurity 허용

--- backend/src/main/java/com/airlock/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/airlock/backend/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.airlock.backend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -11,6 +12,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.List;
 
+@EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
